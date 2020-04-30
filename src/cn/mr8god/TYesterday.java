249fab05c6336b20b29865b62d7a5a317498531a
Mr8god <17696748602@163.com>
2020-04-30 23:19:56 +0800

    test

---
+++ src/cn/mr8god/TYesterday.java
@@ -0,0 +1,18 @@
+package cn.mr8god;
+
+/**
+ * @author
+ * @date
+ * @time
+ */
+public class TYesterday {
+    public static void main(String[] args) {
+//        StringBuffer s = new StringBuffer();
+//        s.append("ABCD");
+//        System.out.println(s);
+
+        System.out.println((new String("EFGH")).toLowerCase());
+
+        System.out.println(((new StringBuffer()).append("ABCD")).toString().toLowerCase());
+    }
+}
